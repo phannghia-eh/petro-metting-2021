1120d69b651fb2a355eff16618293cb623f78522
Phan Nghia <nghia.phan@employmenthero.com>
2021-12-25 08:53:15 -0800

    update model

--- core/participant.service.js
+++ core/participant.service.js
@@ -3,10 +3,22 @@ const Participant = require('./participant.model')
 class ParticipantService {
     static async create(participant) {
         return new Promise((resolve, reject) => {
-            const newParticipant = new Participant(participant)
-            newParticipant.save((err, rls) => {
-                if (err) return reject(err)
-                return resolve(rls)
+            Participant.findOneAndUpdate(participant.email, participant, {upsert: true}, function (error, result) {
+                if (!error) {
+                    // If the document doesn't exist
+                    if (!result) {
+                        // Create it
+                        result = new Participant(participant);
+                    }
+                    // Save the document
+                    result.save(function(error) {
+                        if (!error) {
+                            return resolve(result)
+                        } else {
+                            return reject(error);
+                        }
+                    });
+                }
             })
         })
     }
